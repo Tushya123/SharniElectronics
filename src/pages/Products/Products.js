17b33072f815dabb326d8aac0dcb1c54b461439a
Tushya123 <tushyagandhi123@gmail.com>
2024-05-17 13:37:57 +0530

    changes

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -71,16 +71,23 @@ console.log("id",id)
     try {
        const response = await axios.post(`${process.env.REACT_APP_API_URL_SHREEJI_PHARMACY}/api/auth/create/InquiryProduct`, productData);
       console.log('Response:', response.data.data._id);
-      let productIds = JSON.parse(localStorage.getItem('productIds')) || [];
+      const productId = response.data.data._id;
+      console.log('Product ID from response:', productId);
+
+      // Retrieve existing product IDs from localStorage
+      let productIds = JSON.parse(localStorage.getItem('productIds'));
+      console.log('Current product IDs in localStorage:', productIds);
 
       // Add the new product ID to the array
-      productIds.push(response.data.data._id);
+      productIds.push(productId);
+      console.log('Updated product IDs:', productIds);
 
-      // Save the updated array back to local storage
+      // Save the updated array back to localStorage
       localStorage.setItem('productIds', JSON.stringify(productIds));
-      setQuantity(0);
+      console.log('Updated product IDs saved to localStorage');
 
-      
+      // Reset the quantity
+      setQuantity(0);
     } catch (error) {
       console.error('Error adding product to cart:', error);
     }
