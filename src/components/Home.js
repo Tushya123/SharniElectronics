3f8a978e02b0561e3ce456d1d3ca8dc7d727ee47
avniahir <avni.empbweb@gmail.com>
2024-06-14 11:03:48 +0530

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -66,7 +66,7 @@ export default function Home() {
     arrows: true,
     infinite: true,
     speed: 500,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -76,7 +76,7 @@ export default function Home() {
           dots={dots}
           numDotsToShow={4}
           dotWidth={30}
-          autoplay={true}
+          // autoplay={true}
           speed={500}
           slidesToScroll={1}
           slidesToShow={1}
