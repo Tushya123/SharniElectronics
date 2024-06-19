23c066b6497ad439edaac08f13a648a8f009f4a0
avniahir <avni.empbweb@gmail.com>
2024-06-19 12:45:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       <SearchProvider>
         <BrowserRouter>
           <Header />
-          <ScrollToTop/>
+          {/* <ScrollToTop/> */}
           <>
             <Routes>
               <Route path="/" element={<Home />} />
